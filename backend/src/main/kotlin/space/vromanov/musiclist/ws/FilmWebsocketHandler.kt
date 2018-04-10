d7bc0fd847a54505a8f11bdfdc4517d1dcd5315f
Valery Romanov <ikrakozabra@gmail.com>
2018-04-11 00:24:18 +0300

    (B) Created a significant part of messaging logic

--- backend/src/main/kotlin/space/vromanov/musiclist/ws/FilmWebsocketHandler.kt
+++ backend/src/main/kotlin/space/vromanov/musiclist/ws/FilmWebsocketHandler.kt
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
-import space.vromanov.musiclist.data.Film
 import space.vromanov.musiclist.data.FilmService
 
 /**
@@ -19,60 +18,26 @@ class FilmWebsocketHandler(
     @Autowired private val filmService: FilmService,
     @Autowired private val objectMapper: ObjectMapper
 ): TextWebSocketHandler() {
-    companion object {
-        // Success codes for outgoing message:
-        val ALL_ENTRIES_FETCH_SUCCESS = 100
-        val FILM_ADD_SUCCESS = 101
-        val FILM_MODIFY_SUCCESS = 102
-        val FILM_DELETE_SUCCESS = 103
-        // Failure codes for outgoing message:
-        val ALL_ENTRIES_FETCH_FAILURE = 200
-        val FILM_ADD_FAILURE = 201
-        val FILM_MODIFY_FAILURE = 202
-        val FILM_DELETE_FAILURE = 203
-    }
 
     /**
      * Contains all web socket messaging logic connected with actions with film object.
-     * TODO make a class for outgoing messages which would be like { "code": 1|"OK"|"SUCCESS", "message": "success message" }
      */
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         try {
             val msg = objectMapper.readValue<WsClientMessage>(message.payload)
+            val routeHelper = WebSocketRouteHelper(session, msg, filmService, objectMapper)
             when (msg.action) {
-                MessageAction.ADD -> {
-                    val film = filmService.addFilm(readFilmObject(message))
-                    send("New film added: ${objectMapper.writeValueAsString(film)}", session)
-                }
-                MessageAction.MODIFY -> {
-                    val film = filmService.updateFilm(readFilmObject(message))
-                    send("Film updated: ${objectMapper.writeValueAsString(film)}", session)
-                }
-                MessageAction.DELETE -> {
-                    val filmId = msg.payload.toLongOrNull(10)
-                    if (filmId == null) {
-                        send("Error: can't parse film's ID", session)
-                    } else {
-                        filmService.deleteFilm(filmId)
-                        send("Successfully deleted film with id $filmId", session)
-                    }
-                }
-                else -> send("Not implemented yet", session)
+                MessageAction.FETCH_ALL -> routeHelper.processFetchAllMessage()
+                MessageAction.ADD       -> routeHelper.processAddFilmMessage()
+                MessageAction.MODIFY    -> routeHelper.processModifyFilmMessage()
+                MessageAction.DELETE    -> routeHelper.processDeleteFilmMessage()
             }
-        } catch (error: JsonProcessingException) {
-            send("Incorrect message format. Received message: `${message.payload}`", session)
-        } catch (error: Exception) {
-            send("Error during message processing:\n$error", session)
+        } catch (e: JsonProcessingException) {
+            session.sendMessage(TextMessage(objectMapper.writeValueAsString(
+                WsServerMessage(WebSocketRouteHelper.INVALID_MESSAGE_FORMAT, "Invalid message format:\n${e.message}"))))
+        } catch (e: Exception) {
+            session.sendMessage(TextMessage(objectMapper.writeValueAsString(
+                WsServerMessage(WebSocketRouteHelper.UNKNOWN_ERROR, "Unknown error:\n${e.message}"))))
         }
     }
-
-    /**
-     * A tiny shortcut to session.sendMessage(TextMessage(string))
-     */
-    fun send(msg: String, session: WebSocketSession) = session.sendMessage(TextMessage(msg))
-
-    /**
-     * Uses Jackson's ObjectMapper to retrieve film object from message's payload
-     */
-    fun readFilmObject(msg: TextMessage): Film = objectMapper.readValue<Film>(msg.payload)
 }
--- backend/src/main/kotlin/space/vromanov/musiclist/ws/WsServerMessage.kt
+++ backend/src/main/kotlin/space/vromanov/musiclist/ws/WsServerMessage.kt
@@ -1,9 +1,21 @@
 package space.vromanov.musiclist.ws
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import space.vromanov.musiclist.data.Film
+
 /**
  * Outgoing web socket message to client
  */
 data class WsServerMessage(
     val code: Int,
-    val message: String
-) {}
+    val message: String?
+) {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    var filmEntries: MutableIterable<Film>? = null
+
+    constructor(code: Int) : this(code, null)
+
+    constructor(code: Int, filmEntries: MutableIterable<Film>) : this(code) {
+        this.filmEntries = filmEntries
+    }
+}
